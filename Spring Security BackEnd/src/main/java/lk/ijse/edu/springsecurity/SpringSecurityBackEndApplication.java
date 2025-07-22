56e919c214e4d35f17167010e7ba3b98d336153c
KaveeN <vpdkkaveen@gmail.com>
2025-07-22 16:15:44 +0530

    initialized Spring Security BackEnd project with Maven wrapper setup, database configuration, JWT properties, necessary dependencies, and base structure

---
+++ Spring Security BackEnd/src/main/java/lk/ijse/edu/springsecurity/SpringSecurityBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.edu.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBackEndApplication.class, args);
+    }
+
+}
---
+++ Spring Security BackEnd/src/test/java/lk/ijse/edu/springsecurity/SpringSecurityBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.edu.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
